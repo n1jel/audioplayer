7598763d6f3e9a233994dcfc260f7b54ad60b827
John <john@192.168.0.23>
2023-05-03 13:58:38 +0530

    player setup

--- service.js
+++ service.js
@@ -2,6 +2,9 @@ import TrackPlayer from "react-native-track-player";
 module.exports = async function () {
     // This service needs to be registered for the module to work
     // but it will be used later in the "Receiving Events" section
+    TrackPlayer.setupPlayer().then(() => {
+        console.log('Track player ready');
+    });
     TrackPlayer.addEventListener('remote-play', () =>
         TrackPlayer.play());
     TrackPlayer.addEventListener('remote-pause', () =>
--- src/screens/nonAuth/SoundPlayer.js
+++ src/screens/nonAuth/SoundPlayer.js
@@ -4,53 +4,55 @@ import TrackPlayer, { AppKilledPlaybackBehavior, Capability, RepeatMode, State }
 const SoundPlayer = () => {
     const [isPlayerReady, setIsPlayerReady] = useState(false);
 
+    // useEffect(() => {
+    //     async function setup() {
+    //         let isSetup = await setupPlayer();
+    //         const queue = await TrackPlayer.getQueue();
+    //         if (isSetup && queue.length <= 0) {
+    //             await addTracks();
+    //         }
+    //         setIsPlayerReady(isSetup);
+    //     }
+
+    //     setup();
+    // }, []);
+
+    // async function setupPlayer() {
+    //     let isSetup = false;
+    //     try {
+    //         await TrackPlayer.getCurrentTrack();
+    //         isSetup = true;
+    //     } catch {
+    //         await TrackPlayer.setupPlayer();
+    //         await TrackPlayer.updateOptions({
+    //             android: {
+    //                 appKilledPlaybackBehavior:
+    //                     AppKilledPlaybackBehavior.StopPlaybackAndRemoveNotification,
+    //             },
+    //             capabilities: [
+    //                 Capability.Play,
+    //                 Capability.Pause,
+    //                 Capability.SkipToNext,
+    //                 Capability.SkipToPrevious,
+    //                 Capability.SeekTo,
+    //             ],
+    //             compactCapabilities: [
+    //                 Capability.Play,
+    //                 Capability.Pause,
+    //                 Capability.SkipToNext,
+    //             ],
+    //             progressUpdateEventInterval: 2,
+    //         });
+
+    //         isSetup = true;
+    //     }
+    //     finally {
+    //         return isSetup;
+    //     }
+    // }
     useEffect(() => {
-        async function setup() {
-            let isSetup = await setupPlayer();
-            const queue = await TrackPlayer.getQueue();
-            if (isSetup && queue.length <= 0) {
-                await addTracks();
-            }
-            setIsPlayerReady(isSetup);
-        }
-
-        setup();
-    }, []);
-
-    async function setupPlayer() {
-        let isSetup = false;
-        try {
-            await TrackPlayer.getCurrentTrack();
-            isSetup = true;
-        }
-        catch {
-            await TrackPlayer.setupPlayer();
-            await TrackPlayer.updateOptions({
-                android: {
-                    appKilledPlaybackBehavior:
-                        AppKilledPlaybackBehavior.StopPlaybackAndRemoveNotification,
-                },
-                capabilities: [
-                    Capability.Play,
-                    Capability.Pause,
-                    Capability.SkipToNext,
-                    Capability.SkipToPrevious,
-                    Capability.SeekTo,
-                ],
-                compactCapabilities: [
-                    Capability.Play,
-                    Capability.Pause,
-                    Capability.SkipToNext,
-                ],
-                progressUpdateEventInterval: 2,
-            });
-
-            isSetup = true;
-        }
-        finally {
-            return isSetup;
-        }
-    }
+        addTracks()
+    }, [])
     const addTracks = async () => {
         await TrackPlayer.add([
             {
@@ -61,14 +63,13 @@ const SoundPlayer = () => {
                 duration: 60,
             }
         ]);
-        // await TrackPlayer.setRepeatMode(RepeatMode.Queue);
+        //repeats the queue
+        // await TrackPlayer.setRepeatMode(RepeatMode.Queue); 
     }
 
     return (
         <SafeAreaView>
-            {!isPlayerReady &&
-                <ActivityIndicator size="large" color="#bbb" />
-            }
+            {!isPlayerReady && <ActivityIndicator size="large" color="#bbb" />}
             <Pressable style={{ backgroundColor: "red", height: 100, width: 100 }}
                 onPress={() => {
                     TrackPlayer.play()
